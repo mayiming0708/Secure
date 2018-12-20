fe9902520040544a7665454f0a7b93b85d3dfc36
cwq <513545491@qq.com>
2018-12-20 17:12:03 +0800

    Http工具类

---
+++ src/main/java/com/shtel/secure/utils/HttpUtils.java
@@ -0,0 +1,279 @@
+package com.shtel.secure.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.*;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * @Author: chenwq
+ * @Date: 2018/12/20
+ * @Description: Http工具类
+ */
+public class HttpUtils {
+
+    // 编码格式。发送编码格式统一用UTF-8
+    private static final String ENCODING = "UTF-8";
+
+    // 设置连接超时时间，单位毫秒。
+    private static final int CONNECT_TIMEOUT = 6000;
+
+    // 请求获取数据的超时时间(即响应时间)，单位毫秒。
+    private static final int SOCKET_TIMEOUT = 6000;
+
+    /**
+     * <p>发送get请求；不带请求参数</p>
+     *
+     * @param url 请求地址
+     * @return
+     * @throws Exception
+     */
+    public static JSONObject doGet(String url) throws Exception {
+        return doGet(url, null, null);
+    }
+    /**
+     * <p>发送get请求；带请求参数</p>
+     *
+     * @param url    请求地址
+     * @param params 请求参数集合
+     * @return
+     * @throws Exception
+     */
+    public static JSONObject doGet(String url, Map<String, String> params) throws Exception {
+        return doGet(url, null, params);
+    }
+
+    /**
+     * <p>发送get请求；带请求头和请求参数</p>
+     *
+     * @param url     请求地址
+     * @param headers 请求头集合
+     * @param params  请求参数集合
+     * @return
+     * @throws Exception
+     */
+    public static JSONObject doGet(String url, Map<String, String> headers, Map<String, String> params) throws Exception {
+        // 创建httpClient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        // 创建访问的地址
+        URIBuilder uriBuilder = new URIBuilder(url);
+        if (params != null) {
+            Set<Entry<String, String>> entrySet = params.entrySet();
+            for (Entry<String, String> entry : entrySet) {
+                uriBuilder.setParameter(entry.getKey(), entry.getValue());
+            }
+        }
+
+        // 创建http对象
+        HttpGet httpGet = new HttpGet(uriBuilder.build());
+        /**
+         * setConnectTimeout：设置连接超时时间，单位毫秒。
+         * setConnectionRequestTimeout：设置从connect Manager(连接池)获取Connection
+         * 超时时间，单位毫秒。这个属性是新加的属性，因为目前版本是可以共享连接池的。
+         * setSocketTimeout：请求获取数据的超时时间(即响应时间)，单位毫秒。 如果访问一个接口，多少时间内无法返回数据，就直接放弃此次调用。
+         */
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(CONNECT_TIMEOUT).setSocketTimeout(SOCKET_TIMEOUT).build();
+        httpGet.setConfig(requestConfig);
+
+        // 设置请求头
+        packageHeader(headers, httpGet);
+
+        // 创建httpResponse对象
+        CloseableHttpResponse httpResponse = null;
+
+        try {
+            // 执行请求并获得响应结果
+            return getHttpClientResult(httpResponse, httpClient, httpGet);
+        } finally {
+            // 释放资源
+            release(httpResponse, httpClient);
+        }
+    }
+
+    /**
+     * <p>发送post请求；不带请求头和请求参数</p>
+     *
+     * @param url 请求地址
+     * @return
+     * @throws Exception
+     */
+    public static JSONObject doPost(String url) throws Exception {
+        return doPost(url, null, null);
+    }
+
+    /**
+     * <p>发送post请求；带请求参数</p>
+     *
+     * @param url    请求地址
+     * @param params 参数集合
+     * @return
+     * @throws Exception
+     */
+    public static JSONObject doPost(String url, Map<String, String> params) throws Exception {
+        return doPost(url, null, params);
+    }
+
+    /**
+     * 发送post请求；带请求头和请求参数
+     *
+     * @param url     请求地址
+     * @param headers 请求头集合
+     * @param params  请求参数集合
+     * @return
+     * @throws Exception
+     */
+    public static JSONObject doPost(String url, Map<String, String> headers, Map<String, String> params) throws Exception {
+        // 创建httpClient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        // 创建http对象
+        HttpPost httpPost = new HttpPost(url);
+        /**
+         * setConnectTimeout：设置连接超时时间，单位毫秒。
+         * setConnectionRequestTimeout：设置从connect Manager(连接池)获取Connection
+         * 超时时间，单位毫秒。这个属性是新加的属性，因为目前版本是可以共享连接池的。
+         * setSocketTimeout：请求获取数据的超时时间(即响应时间)，单位毫秒。 如果访问一个接口，多少时间内无法返回数据，就直接放弃此次调用。
+         */
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(CONNECT_TIMEOUT).setSocketTimeout(SOCKET_TIMEOUT).build();
+        httpPost.setConfig(requestConfig);
+        // 设置请求头
+		/*httpPost.setHeader("Cookie", "");
+		httpPost.setHeader("Connection", "keep-alive");
+		httpPost.setHeader("Accept", "application/json");
+		httpPost.setHeader("Accept-Language", "zh-CN,zh;q=0.9");
+		httpPost.setHeader("Accept-Encoding", "gzip, deflate, br");
+		httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");*/
+        packageHeader(headers, httpPost);
+
+        // 封装请求参数
+        packageParam(params, httpPost);
+
+        // 创建httpResponse对象
+        CloseableHttpResponse httpResponse = null;
+
+        try {
+            // 执行请求并获得响应结果
+            return getHttpClientResult(httpResponse, httpClient, httpPost);
+        } finally {
+            // 释放资源
+            release(httpResponse, httpClient);
+        }
+    }
+
+    /**
+     * <p>封装请求头</p>
+     *
+     * @param params
+     * @param httpMethod
+     */
+    public static void packageHeader(Map<String, String> params, HttpRequestBase httpMethod) {
+        // 封装请求头
+        if (params != null) {
+            Set<Entry<String, String>> entrySet = params.entrySet();
+            for (Entry<String, String> entry : entrySet) {
+                // 设置到请求头到HttpRequestBase对象中
+                httpMethod.setHeader(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    /**
+     * <p>封装请求参数</p>
+     *
+     * @param params
+     * @param httpMethod
+     * @throws UnsupportedEncodingException
+     */
+    public static void packageParam(Map<String, String> params, HttpEntityEnclosingRequestBase httpMethod) throws UnsupportedEncodingException {
+        // 封装请求参数
+        if (params != null) {
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            Set<Entry<String, String>> entrySet = params.entrySet();
+            for (Entry<String, String> entry : entrySet) {
+                nvps.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+            }
+            // 设置到请求的http对象中
+            httpMethod.setEntity(new UrlEncodedFormEntity(nvps, ENCODING));
+        }
+    }
+
+    /**
+     * <p>封装响应结果</p>
+     *
+     * @param httpResponse
+     * @param httpClient
+     * @param httpMethod
+     * @return
+     * @throws Exception
+     */
+    public static JSONObject getHttpClientResult(CloseableHttpResponse httpResponse, CloseableHttpClient httpClient, HttpRequestBase httpMethod) throws Exception {
+        // 执行请求
+        httpResponse = httpClient.execute(httpMethod);
+
+        // 获取返回结果
+        if (httpResponse != null && httpResponse.getStatusLine() != null) {
+            String content = "";
+            if (httpResponse.getEntity() != null) {
+                content = EntityUtils.toString(httpResponse.getEntity(), ENCODING);
+            }
+            return JsonResult(httpResponse.getStatusLine().getStatusCode(), content);
+        }
+        return JsonResult(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+    }
+
+
+    /**
+     * <p>释放资源</p>
+     *
+     * @param httpResponse
+     * @param httpClient
+     * @throws IOException
+     */
+    public static void release(CloseableHttpResponse httpResponse, CloseableHttpClient httpClient) throws IOException {
+        // 释放资源
+        if (httpResponse != null) {
+            httpResponse.close();
+        }
+        if (httpClient != null) {
+            httpClient.close();
+        }
+    }
+
+    /**
+     * <p>响应结果封装</p>
+     *
+     * @param code    状态码
+     * @param content 响应内容
+     * @return
+     */
+    public static JSONObject JsonResult(int code, String content) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", code);
+        jsonObject.put("content", content);
+        return jsonObject;
+    }
+
+    public static JSONObject JsonResult(int code) {
+        return JsonResult(code, null);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(doGet("http://172.31.27.48:8085/idealfolder/styleCover/2018121409/1544752290704.jpg",null));
+    }
+}
