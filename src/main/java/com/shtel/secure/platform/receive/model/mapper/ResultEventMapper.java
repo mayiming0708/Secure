93d672e9392964253045171f83955076e966d4f3
MAYIMING <mayiming0708@vip.qq.com>
2019-01-17 18:58:34 +0800

    优化查询方法

--- src/main/java/com/shtel/secure/platform/receive/model/mapper/ResultEventMapper.java
+++ src/main/java/com/shtel/secure/platform/receive/model/mapper/ResultEventMapper.java
@@ -45,11 +45,9 @@ public interface ResultEventMapper extends Mapper<ResultEvent> {
             "\tws_result_event.site,\n" +
             "\tws_result_event.end_at as endAt\n" +
             "FROM\n" +
-            "\tws_task,\n" +
-            "\tws_result_event \n" +
+            "\tws_result_event LEFT JOIN ws_task on ws_task.virtual_group_id = ws_result_event.virtual_group_id\n" +
             "WHERE\n" +
             "\tws_task.user_id = #{userId} \n" +
-            "\tAND ws_task.virtual_group_id = ws_result_event.virtual_group_id \n" +
             "\tAND ws_result_event.site = #{site}")
     List<ResultEvent> getResultEventsByUserIdAndUrl(@Param("userId") Integer userId, @Param("site") String site);
 }
